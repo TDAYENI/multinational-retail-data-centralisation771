8036a1b9835270880000579689ad2b41ded5c9ed
TDAYENI <titilayoay@googlemail.com>
2024-06-22 15:16:21 +0100

    read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,13 @@
+import yaml
 # Todo which you will use to connect with and upload data to the database
-class DataConnector :
-    pass
+
+
+class DataConnector:
+    def read_db_creds():
+        with open('cred/db_creds.yaml') as file:
+            db_cred = yaml.safe_load(file)
+        return db_cred
+
+
+inst_connect = DataConnector.read_db_creds()
+print(inst_connect)
