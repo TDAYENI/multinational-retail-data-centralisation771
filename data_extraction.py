156d0d62a6f3de98db1904aab5775ca89c44ce5c
TDAYENI <titilayoay@googlemail.com>
2024-06-22 13:53:58 +0100

    added cred yaml file

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,6 @@
 
-#Todo The methods contained will be fit to extract data from a particular data source, 
-#Todo these sources will include CSV files, an API and an S3 bucket
+# Todo The methods contained will be fit to extract data from a particular data source,
+# Todo these sources will include CSV files, an API and an S3 bucket
+
 class DataExtractor:
     pass
-    
