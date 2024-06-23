4e6c5061b5cb9f0d19c0fdf01dbb0452e975f7df
TDAYENI <titilayoay@googlemail.com>
2024-06-23 20:39:01 +0100

    alchemy engine redundant code removed

--- database_utils.py
+++ database_utils.py
@@ -8,23 +8,29 @@ class DataConnector:
     def __init__(self):
         self.db_cred = None
 
-    def read_db_creds(self, cred_file_path):
 
+    def read_db_creds(self, cred_file_path):
+        # reads database credentials then initaites method to create db connection
         with open(cred_file_path, 'r') as file:
             self.db_cred = yaml.safe_load(file)
         return self.init_db_engine(self.db_cred)
     
     def init_db_engine(self, db_cred):
+        # return an sqlalchemy database engine
         DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        HOST = db_cred['RDS_HOST']
-        USER = db_cred['RDS_USER']
-        PASSWORD = db_cred['RDS_PASSWORD']
-        DATABASE = db_cred['RDS_DATABASE']
-        PORT = db_cred['RDS_PORT']
-        engine = create_engine(
-            f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-        print(engine)
+        DBAPI = 'psycopg2' 
+        alchemy_engine = create_engine(
+            f"{DATABASE_TYPE}+{DBAPI}://{db_cred['RDS_USER']}:"
+            f"{db_cred['RDS_PASSWORD']}@{db_cred['RDS_HOST']}:"
+            f"{db_cred['RDS_PORT']}/{db_cred['RDS_DATABASE']}"
+            )
+        print(alchemy_engine)
+        return alchemy_engine
+    
+
+    def list_db_tables (self):
+        inspector = inspect(engine)
+        return(print(inspector.get_table_names()))
 
 
 inst_connect = DataConnector()
