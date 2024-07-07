6a91ce0558669042d88d385732824cd1ecaff1a1
TDAYENI <titilayoay@googlemail.com>
2024-07-07 20:33:50 +0100

    Corrected Cred files and upload info points to right location in pg admin

--- main.py
+++ main.py
@@ -11,28 +11,22 @@ def main():
     inst_connect.list_db_tables()
 
     extractor = DataExtractor()
-    #returns data frame of leagcy users
+    # returns data frame of leagcy users
     legacy_users_df = extractor.read_rds_table('legacy_users', aws_engine)
-    #returns cleaned data to be uploaded to 
+    # returns cleaned data to be uploaded to
     legacy_users_clean = DataCleaning()
     cleaned_users_df = legacy_users_clean.clean_user_data(legacy_users_df)
 
-    #retrieve database cred to upload 
+    # retrieve database cred to upload to datasales
     pg_admin_inst = DataConnector()
     pg_admin_engine = pg_admin_inst.read_db_creds('cred/pg_admin_creds.yaml')
     print(pg_admin_engine)
 
     #
     pg_admin_inst.upload_to_db(
-        pg_admin_engine, 'legacy_users', cleaned_users_df)
+        pg_admin_engine, 'dim_users', cleaned_users_df)
 
-
-
-
-    
     # legacy_users = data_extractor.read_rds_table('legacy_users')
     # print(legacy_users.head())
-
-
 if __name__ == "__main__":
     main()
