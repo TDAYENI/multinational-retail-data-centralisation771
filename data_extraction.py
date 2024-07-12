18a085057eeb5159275393fa3af4014b57fbe198
TDAYENI <titilayoay@googlemail.com>
2024-07-12 19:31:52 +0100

    list_number_of_stores works

--- data_extraction.py
+++ data_extraction.py
@@ -1,17 +1,19 @@
 
 
+import requests
 from sqlalchemy import inspect
 from sqlalchemy import text
 import pandas as pd
 import tabula
+import boto3
+import requests
 
 # Todo The methods contained will be fit to extract data from a particular data source,
 # Todo these sources will include CSV files, an API and an S3 bucket
 
 
 class DataExtractor:
-    def __init__(self):
-        pass
+
     # class to read the data from the RDS database
 
     def read_rds_table(self, table_name, engine):
@@ -21,11 +23,58 @@ class DataExtractor:
     # Todo extract the database table to a pandas DataFrame
     #  TODO ke in an instance of your DatabaseConnector class and
     # TODO the table name as an argument and return a pandas DataFrame.
-    def retrieve_pdf_data(self,link):
-        pdf_df = tabula.read_pdf(link,pages='all') 
+    def retrieve_pdf_data(self, link):
+        pdf_df = tabula.read_pdf(link, pages='all')
         pandas_pdf = pd.concat(pdf_df, ignore_index=True)
         return pandas_pdf
 
-    def list_number_of_stores(self,header):
-        
+    def list_number_of_stores(self, url, header):
 
+        try:
+            response = requests.get(url, headers=header)
+            # This will raise an HTTPError for bad responses (4xx and 5xx)
+            response.raise_for_status()
+
+            # Process the response if no exceptions were raised
+            data = response.json()  # Assuming the response is in JSON format
+            print(data)
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.ConnectionError as conn_err:
+            print(f"Connection error occurred: {conn_err}")
+        except requests.exceptions.Timeout as timeout_err:
+            print(f"Timeout error occurred: {timeout_err}")
+        except requests.exceptions.RequestException as req_err:
+            print(f"An error occurred: {req_err}")
+        except ValueError as json_err:
+            print(f"JSON decoding failed: {json_err}")
+        return data
+
+    def retrieve_stores_data(store_number, header):
+        store_data = []
+
+        for store_number_index in range(0, store_number):
+
+            try:
+                response = requests.get(
+                    f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number_index}', headers=header)
+                # This will raise an HTTPError for bad responses (4xx and 5xx)
+                response.raise_for_status()
+
+                # Process the response if no exceptions were raised
+                data = response.json()  # Assuming the response is in JSON format
+                store_data.append(data)
+
+            except requests.exceptions.HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err}")
+            except requests.exceptions.ConnectionError as conn_err:
+                print(f"Connection error occurred: {conn_err}")
+            except requests.exceptions.Timeout as timeout_err:
+                print(f"Timeout error occurred: {timeout_err}")
+            except requests.exceptions.RequestException as req_err:
+                print(f"An error occurred: {req_err}")
+            except ValueError as json_err:
+                print(f"JSON decoding failed: {json_err}")
+
+        return pd.DataFrame(store_data)
--- main.py
+++ main.py
@@ -4,80 +4,90 @@ from data_cleaning import DataCleaning
 import pandas as pd 
 
 def main():
-    #* Reading DB
-    # reads database creds and returns engine
-    inst_connect = DataConnector()
-    aws_engine = inst_connect.read_db_creds('cred/db_creds.yaml')
+    # #* Reading DB
+    # # reads database creds and returns engine
+    # inst_connect = DataConnector()
+    # aws_engine = inst_connect.read_db_creds('cred/db_creds.yaml')
 
-    inst_connect.list_db_tables()
-    # returns data frame of leagcy users
-    extractor = DataExtractor()
-    legacy_users_df = extractor.read_rds_table('legacy_users', aws_engine)
-    # * Cleaning users dataframe
-    # Cleans users data
-    legacy_users_clean = DataCleaning()
+    # inst_connect.list_db_tables()
+    # # returns data frame of leagcy users
+    # extractor = DataExtractor()
+    # legacy_users_df = extractor.read_rds_table('legacy_users', aws_engine)
+    # # * Cleaning users dataframe
+    # # Cleans users data
+    # legacy_users_clean = DataCleaning()
 
-    # replace nulls
-    cleaned_users = legacy_users_clean.replace_nulls(legacy_users_df)
+    # # replace nulls
+    # cleaned_users = legacy_users_clean.replace_nulls(legacy_users_df)
 
-    # convert_dates to date time
-    date_columns = ['date_of_birth', 'join_date']
-    cleaned_users = legacy_users_clean.convert_dates(
-        legacy_users_df, date_columns)
+    # # convert_dates to date time
+    # date_columns = ['date_of_birth', 'join_date']
+    # cleaned_users = legacy_users_clean.convert_dates(
+    #     legacy_users_df, date_columns)
     
-    # remove non numeric characters
-    cleaned_users = legacy_users_clean.clean_numbers(
-        legacy_users_df, column='phone_number')
-    # sets relevant data types
-    cleaned_users = legacy_users_clean.convert_data_types(legacy_users_df)
-    # drop null
-    cleaned_users = legacy_users_clean.drop_na(legacy_users_df)
+    # # remove non numeric characters
+    # cleaned_users = legacy_users_clean.clean_numbers(
+    #     legacy_users_df, column='phone_number')
+    # # sets relevant data types
+    # cleaned_users = legacy_users_clean.convert_data_types(legacy_users_df)
+    # # drop null
+    # cleaned_users = legacy_users_clean.drop_na(legacy_users_df)
 
-    #* Upload Users Data to DB
-     # retrieve database cred to upload to datasales
-    pg_admin_inst = DataConnector()
-    pg_admin_engine = pg_admin_inst.read_db_creds('cred/pg_admin_creds.yaml')
-    print(pg_admin_engine)
-    pg_admin_inst.upload_to_db(pg_admin_engine, table_name='dim_users',data_frame=cleaned_users)
+    # #* Upload Users Data to DB
+    #  # retrieve database cred to upload to datasales
+    # pg_admin_inst = DataConnector()
+    # pg_admin_engine = pg_admin_inst.read_db_creds('cred/pg_admin_creds.yaml')
+    # print(pg_admin_engine)
+    # pg_admin_inst.upload_to_db(pg_admin_engine, table_name='dim_users',data_frame=cleaned_users)
 
 
-    # TODO from up here ctl /
-    # * PDF Section
-    # Retrieving and creating pdf as pd data frame
-    pdf_inst = DataExtractor()
-    pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
-    pdf_data = pdf_inst.retrieve_pdf_data(link=pdf_link)
-    breakpoint()
-    # * PDF Cleaning
-    pdf_cleaning_inst = DataCleaning()
+    # # TODO from up here ctl /
+    # # * PDF Section
+    # # Retrieving and creating pdf as pd data frame
+    # pdf_inst = DataExtractor()
+    # pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+    # pdf_data = pdf_inst.retrieve_pdf_data(link=pdf_link)
+    # breakpoint()
+    # # * PDF Cleaning
+    # pdf_cleaning_inst = DataCleaning()
 
-    pdf_cleaned = pdf_cleaning_inst.replace_nulls(pdf_data)
+    # pdf_cleaned = pdf_cleaning_inst.replace_nulls(pdf_data)
 
-    dates_list = ['date_payment_confirmed']
-    pdf_cleaned = pdf_cleaning_inst.convert_dates(
-        pdf_cleaned, date_columns=dates_list)
-    # TODO change date of confirmed ID
-    pdf_cleaned = pdf_cleaning_inst.clean_numbers(
-        pdf_cleaned, column='card_number')
+    # dates_list = ['date_payment_confirmed']
+    # pdf_cleaned = pdf_cleaning_inst.convert_dates(
+    #     pdf_cleaned, date_columns=dates_list)
+    # # TODO change date of confirmed ID
+    # pdf_cleaned = pdf_cleaning_inst.clean_numbers(
+    #     pdf_cleaned, column='card_number')
     
-    pdf_cleaned = pdf_cleaning_inst.drop_na(pdf_cleaned)
+    # pdf_cleaned = pdf_cleaning_inst.drop_na(pdf_cleaned)
     
-    # * Upload PDF Data to DB
-    # retrieve database cred to upload to datasales
+    # # * Upload PDF Data to DB
+    # # retrieve database cred to upload to datasales
     
-    pg_admin_inst.upload_to_db(
-        pg_admin_engine, table_name='dim_card_details', data_frame=pdf_cleaned)
+    # pg_admin_inst.upload_to_db(
+    #     pg_admin_engine, table_name='dim_card_details', data_frame=pdf_cleaned)
+    
+    #TODO from here up
+    #* Retrieve Product data
+    aws_header =  {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
+    store_num_url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+    store_num_inst = DataExtractor()
+    num_of_stores = store_num_inst.list_number_of_stores(url=store_num_url,header=aws_header)
+    
+    store_num_inst.retrieve_stores_data(store_number =num_of_stores,header=aws_header)
 
-    # cleaned_users_df = legacy_users_clean.clean_user_data(legacy_users_df.info)
 
 
+ 
+if __name__ == "__main__":
+    main()
+
+   # cleaned_users_df = legacy_users_clean.clean_user_data(legacy_users_df.info)
 
     # #
     # pg_admin_inst.upload_to_db(
     #     pg_admin_engine, 'dim_users', cleaned_users_df)
 
-
     # legacy_users = data_extractor.read_rds_table('legacy_users')
     # print(legacy_users.head())
-if __name__ == "__main__":
-    main()
