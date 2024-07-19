991d323dfaa4dcc0f6d222dc42964bb15a8f15f3
TDAYENI <titilayoay@googlemail.com>
2024-07-19 13:16:09 +0100

    - Alter table in  dim_store_details sql works

--- SQL/alter_statements.sql
+++ SQL/alter_statements.sql
@@ -32,13 +32,13 @@ ALTER COLUMN first_name  TYPE  VARCHAR(255),
 ALTER TABLE  dim_store_details
 ALTER COLUMN longitude TYPE  FLOAT USING longitude::double precision,
 	ALTER COLUMN locality  TYPE  VARCHAR(255),
-	ALTER COLUMN store_code TYPE VARCHAR,
+	ALTER COLUMN store_code TYPE VARCHAR(12),
 	ALTER COLUMN staff_numbers TYPE SMALLINT USING staff_numbers::smallint,
 	ALTER COLUMN opening_date    TYPE DATE,
 	ALTER COLUMN store_type  TYPE VARCHAR(255) ,
 	ALTER COLUMN latitude TYPE FLOAT USING latitude::double precision,
-	ALTER COLUMN country_code TYPE VARCHAR,
-	ALTER COLUMN continent    TYPE VARCHAR;
+	ALTER COLUMN country_code TYPE VARCHAR(2),
+	ALTER COLUMN continent    TYPE VARCHAR(255);
 	
 	
 --dim_products change product numbers
