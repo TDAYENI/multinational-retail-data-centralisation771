0bbc148e0e1c821c1a8b202fa67fbbfc6c32597d
TDAYENI <titilayoay@googlemail.com>
2024-07-22 15:48:11 +0100

    Query 1 comp

--- SQL/t1_how_many_Stores_.sql
+++ SQL/t1_how_many_Stores_.sql
@@ -1,2 +1,6 @@
 -- The Operations team would like to know which countries we currently operate in and which country now has the most stores. Perform a query on the database to get the information, it should return the following information:
 
+SELECT country_code, COUNT(*) FROM dim_store_details 
+GROUP BY country_code
+ORDER BY COUNT(*) DESC
+LIMIT 3;
---
+++ SQL/t2_most_stores.sql
@@ -0,0 +1,3 @@
+-- The business stakeholders would like to know which locations currently have the most stores.
+-- They would like to close some stores before opening more in other locations.
+--  Find out which locations have the most stores currently
