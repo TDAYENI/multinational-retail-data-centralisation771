2c367cf48ef766b1389b8909f8e4db06d93604c6
TDAYENI <titilayoay@googlemail.com>
2024-07-22 17:53:21 +0100

    database_utils doc string added

--- database_utils.py
+++ database_utils.py
@@ -3,7 +3,7 @@ import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 
-# Todo which you will use to connect with and upload data to the database
+
 
 
 class DataConnector:
@@ -12,14 +12,32 @@ class DataConnector:
         self.db_cred = None
 
     def read_db_creds(self, cred_file_path):
-        # reads database credentials then initaites
-        #  method to create db connection
+        """_summary_
+         Read the database credentials from a YAML file and initialise the database engine
+        Parameters
+        ----------
+        cred_file_path : (str)
+            _description_
+
+        Returns
+        -------
+         sqlalchemy.engine.base.Engine: The initialized SQLAlchemy engine
+        """
         with open(cred_file_path, 'r') as file:
             self.db_cred = yaml.safe_load(file)
         return self.init_db_engine(self.db_cred)
 
     def init_db_engine(self, db_cred):
-        # return an sqlalchemy database engine
+        """_summary_
+         Initialise a SQLAlchemy engine using the provided database credentials
+        Parameters
+        ----------
+        db_cred : Dictionary containing database credentials
+
+        Returns
+        -------
+          sqlalchemy.engine.base.Engine: The initialized SQLAlchemy engine
+        """
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
         # defining alchemy_engine
@@ -32,27 +50,29 @@ class DataConnector:
         return self.alchemy_engine
 
     def list_db_tables(self):
+        """_summary_
+         List all the tables in  connected database.
+        Parameters
+        Returns
+        -------
+       list:  list of table names in the database.
+        """
         inspector = inspect(self.alchemy_engine)
         return print(inspector.get_table_names())
 
     def upload_to_db(self, engine, table_name, data_frame):
-        # TODO upload_to_db method to store the data
-        # Todo in your sales_data database in a table named dim_users
-        # df.to_sql(table_name, local_engine, if_exists='replace')
+        """_summary_
+        Upload a data to the specified table in  database.
+        Parameters
+        ----------
+        engine : The SQLAlchemy engine connected to the database
+        table_name :(str): The name of the table where the data will be uploaded
+        data_frame :(pandas data frame) The DataFrame containing the data to be uploaded
+        """
         try:
             data_frame.to_sql(table_name, engine,
                               index=False, if_exists='replace')
             print(f"Data uploaded to {table_name} successfully.")
         except Exception as e:
             print(f"Error uploading data to legacy_users: {e}")
-
-
-# aws_connector = DataConnector()
-# aws_engine=aws_connector.read_db_creds('cred/db_creds.yaml')
-# aws_connector.list_db_tables()
-
-# pg engine to push data to database
-# pg_admin = DataConnector()
-# pg_engine=pg_admin.read_db_creds('cred/pg_admin_creds.yaml')
-
-# DataExtractor.read_rds_table('legacy_users', aws_engine)
+            
