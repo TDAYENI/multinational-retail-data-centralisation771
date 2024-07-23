1b939e7a1828ceed9db840d58c0a41001e5e0608
TDAYENI <titilayoay@googlemail.com>
2024-07-23 21:27:07 +0100

    t4 cte offline

--- SQL/t4_on_vs_offline.sql
+++ SQL/t4_on_vs_offline.sql
@@ -4,40 +4,40 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 WITH OnlineData AS (
     SELECT 
         COUNT(*) AS number_of_sales, 
         SUM(o.product_quantity) AS total_product_quantity, 
         'Web' AS location
+    FROM  orders_table o
+        JOIN dim_store_details dsd ON dsd.store_code = o.store_code
+    WHERE 
+        dsd.store_type = 'Web Portal'
+    GROUP BY dsd.store_type
+), 
+OfflineData AS (
+    SELECT COUNT(*) AS number_of_sales, SUM(o.product_quantity) AS total_product_quantity, 
+        'Offline' AS location
     FROM 
         orders_table o
         JOIN dim_store_details dsd ON dsd.store_code = o.store_code
     WHERE 
-        dsd.store_type = 'Web Portal'
+        dsd.store_type != 'Web Portal'
     GROUP BY 
         dsd.store_type
 )
-SELECT * FROM OnlineData;
+SELECT SUM(number_of_sales) AS total_sales,  
+    SUM(total_product_quantity) AS total_product_quantity, 
+    location
+FROM  OnlineData
+GROUP BY 
+    location
 
+UNION
+
+SELECT SUM(number_of_sales) AS total_sales,
+    SUM(total_product_quantity) AS total_product_quantity, 
+    location
+FROM  OfflineData
+GROUP BY 
+    location;
