801f1fe6c916b01d8cb2dfe43b77dbbf3bff0038
TDAYENI <titilayoay@googlemail.com>
2024-07-23 21:30:42 +0100

    t4 complete

--- SQL/t4_on_vs_offline.sql
+++ SQL/t4_on_vs_offline.sql
@@ -3,7 +3,7 @@
 -- Calculate how many products were sold and the amount of sales made for online and offline purchases. --
 
 
-
+--CTE,COUNT no/sum,online sales, join orders group by storetype
 WITH OnlineData AS (
     SELECT 
         COUNT(*) AS number_of_sales, 
@@ -15,17 +15,18 @@ WITH OnlineData AS (
         dsd.store_type = 'Web Portal'
     GROUP BY dsd.store_type
 ), 
+--offline CTE, count num off line sale /sum prod quant
 OfflineData AS (
     SELECT COUNT(*) AS number_of_sales, SUM(o.product_quantity) AS total_product_quantity, 
         'Offline' AS location
     FROM 
-        orders_table o
+        orders_table o -- join store details
         JOIN dim_store_details dsd ON dsd.store_code = o.store_code
     WHERE 
         dsd.store_type != 'Web Portal'
     GROUP BY 
         dsd.store_type
-)
+) 
 SELECT SUM(number_of_sales) AS total_sales,  
     SUM(total_product_quantity) AS total_product_quantity, 
     location
@@ -34,7 +35,7 @@ GROUP BY
     location
 
 UNION
-
+-- agg of offline results
 SELECT SUM(number_of_sales) AS total_sales,
     SUM(total_product_quantity) AS total_product_quantity, 
     location
